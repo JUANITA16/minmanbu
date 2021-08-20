203d646e594b17a624758bffcf4261190b1c4dd2
JAIR RODRIGUEZ RODRIGUEZ <Jair.Rodriguez@btgpactual.com>
2021-08-19 20:17:09 -0500

    Add helpers

---
+++ src/helpers/utils.js
@@ -0,0 +1,32 @@
+import { toast } from 'react-toastify';
+
+export const setError = (error) => {
+    if (error.request) {
+        console.error(error.request);
+        return {
+            detail: "No se pudo generar el archivo, por favor comuníquese con el administrador."
+        }
+    }
+    else if (error.response)
+        console.log(error.response);
+    else if (error.message)
+        console.log(error.message);
+}
+
+export const setDate = (date) => {
+    return new Date(date.getTime() - (date.getTimezoneOffset() * 60000))
+        .toISOString()
+        .split("T")[0];
+}
+
+export const showToast = (message) => {
+    toast.dark(message, {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: false,
+        progress: undefined,
+    });
+}
