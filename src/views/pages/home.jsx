c18322351b42167098f09bf8928b30fde6dc8f61
Martinez <Camilo.Martinez@btgpactual.com>
2021-11-10 17:06:57 -0500

    version

--- src/views/pages/home.jsx
+++ src/views/pages/home.jsx
@@ -5,7 +5,7 @@ import { Row, Col } from 'react-materialize'
 
 export default function Home() {
   const base = process.env.PUBLIC_URL;
-  const title = 'Inicio';
+  const title = 'Inicio - Versión 2';
   const description = 'Mini mambu pagina princial.';
 
   useEffect(() => {
