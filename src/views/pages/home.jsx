77a6f07bb3d747dd75be94afe5131a91db5c5940
Martinez <Camilo.Martinez@btgpactual.com>
2021-11-30 08:06:08 -0500

    corección ortografía

--- src/views/pages/home.jsx
+++ src/views/pages/home.jsx
@@ -6,7 +6,7 @@ import { Row, Col } from 'react-materialize'
 export default function Home() {
   const base = process.env.PUBLIC_URL;
   const title = 'Inicio';
-  const description = 'Mini mambu pagina princial.';
+  const description = 'Mini mambu p&aacute;gina principal.';
 
   useEffect(() => {
     document.title = title
