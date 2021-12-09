7e7d165cceae26712159a09aed7bb4346dd2c528
Neyra <Brayan.Neyra@btgpactual.com>
2021-12-09 10:11:03 -0500

    Prueba de concepto para descargar

--- src/views/pages/generate-sap.jsx
+++ src/views/pages/generate-sap.jsx
@@ -33,14 +33,15 @@ export default function GenerateSap() {
     if (response !== '') {
       setInProgress(() => false);
       showToast(() => response);
-      serviceDownload();
+      // serviceDownload();
+      download();
     }
   }, [response])
 
   function download() {
     console.log('fileName download:' + fileName);
     // setContenFile(() => 'Ejemplo de text\n Despues otra linea');
-    // contentFile = 'Ejemplo de text\n Despues otra linea';
+    contentFile = 'Ejemplo de text\n Despues otra linea';
     const element = document.createElement("a");
     const file = new Blob([contentFile], { type: 'text/plain;charset-utf-8' });
     element.href = URL.createObjectURL(file);
