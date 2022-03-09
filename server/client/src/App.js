33ccd297b1fc4a201370460296622e242c761747
Restrepo <Juan.Restrepo@btgpactual.com>
2022-03-09 00:34:12 -0500

    Adjust permission roles on client

--- server/client/src/App.js
+++ server/client/src/App.js
@@ -28,12 +28,14 @@ export default function App() {
                   <Route path={base + '/'} exact component={Home} />
                   {(user.roles.includes("MINMAMBU_ROLE_ADMINISTRADOR_DEV") || 
                     user.roles.includes("MINMAMBU_ROLE_CUENTES_MASIVAS_CC_DEV") || 
-                    user.roles.includes("MINMAMBU_ROLE_CUENTAS_MASIVAS_CDT_DEV")) && 
-                    <Route path={base + '/ui-crea-cuenta-deposito'} exact component={CreacionCuenta} />
+                    user.roles.includes("MINMAMBU_ROLE_CUENTAS_MASIVAS_CDT_DEV")) ? 
+                    <Route path={base + '/ui-crea-cuenta-deposito'} exact component={CreacionCuenta} />:
+                    <Route path={base + '/ui-crea-cuenta-deposito'} exact component={Error404} />
                   }
                   {(user.roles.includes("MINMAMBU_ROLE_ADMINISTRADOR_DEV") || 
-                    user.roles.includes("MINMAMBU_ROLE_CONTABILIDAD_DEV")) && 
-                    <Route path={base + '/ui-generate-sap'} exact component={GenerateSap} />
+                    user.roles.includes("MINMAMBU_ROLE_CONTABILIDAD_DEV")) ? 
+                    <Route path={base + '/ui-generate-sap'} exact component={GenerateSap} />:
+                    <Route path={base + '/ui-generate-sap'} exact component={Error404} />
                   }
                 </Switch>
               )}
