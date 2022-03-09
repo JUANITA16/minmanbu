ee7a4dfa21c48db4777d19ac79b374053a3ab198
Restrepo <Juan.Restrepo@btgpactual.com>
2022-03-09 00:30:53 -0500

    Add proxy just in case

---
+++ proxy/src/test/java/com/btgpactual/minmambu/proxy/ProxyMinmambuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.btgpactual.minmambu.proxy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProxyMinmambuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
