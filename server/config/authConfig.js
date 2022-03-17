5d26ad59f75332c71cbcee3c1834276d9140aa80
Restrepo <Juan.Restrepo@btgpactual.com>
2022-03-17 18:18:02 -0500

    Update config/authConfig roles

--- server/config/authConfig.js
+++ server/config/authConfig.js
@@ -18,22 +18,22 @@ const authConfig = async() => {
             massive_cc:{
                 path: "/massive/cc",
                 methods:[ "POST" ],
-                roles:[ "MINMAMBU_ROLE_ADMINISTRADOR_DEV", "MINMAMBU_ROLE_CUENTES_MASIVAS_CC_DEV" ]
+                roles:[ "MINMAMBU_ROLE_ADMINISTRADOR", "MINMAMBU_ROLE_CUENTES_MASIVAS_CC" ]
             },
             massive_cdt:{
                 path: "/massive/cdt",
                 methods:[ "POST" ],
-                roles:[ "MINMAMBU_ROLE_ADMINISTRADOR_DEV", "MINMAMBU_ROLE_CUENTAS_MASIVAS_CDT_DEV" ]
+                roles:[ "MINMAMBU_ROLE_ADMINISTRADOR", "MINMAMBU_ROLE_CUENTAS_MASIVAS_CDT" ]
             },
             table:{
                 path: "/table",
                 methods:[ "GET" ],
-                roles:[ "MINMAMBU_ROLE_ADMINISTRADOR_DEV", "MINMAMBU_ROLE_CUENTES_MASIVAS_CC_DEV", "MINMAMBU_ROLE_CUENTAS_MASIVAS_CDT_DEV" ]
+                roles:[ "MINMAMBU_ROLE_ADMINISTRADOR", "MINMAMBU_ROLE_CUENTES_MASIVAS_CC", "MINMAMBU_ROLE_CUENTAS_MASIVAS_CDT" ]
             },
             sap: {
                 path: "/sap",
                 methods:[ "GET" ],
-                roles:[ "MINMAMBU_ROLE_ADMINISTRADOR_DEV", "MINMAMBU_ROLE_CONTABILIDAD_DEV"]
+                roles:[ "MINMAMBU_ROLE_ADMINISTRADOR", "MINMAMBU_ROLE_CONTABILIDAD"]
             }
         }
     }
