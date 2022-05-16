290b10a892ff55c714cd704d598844fb17556e7a
Colina <Joan.Colina@btgpactual.com>
2022-05-16 16:45:11 -0500

    ADD - funcionalidad de actualziar configuracion general

---
+++ server/routes/configuracion-general.js
@@ -0,0 +1,34 @@
+const router = require('express').Router();
+const axios = require('axios');
+const { getSecret } = require('../utils/secret');
+
+router.get('/datacontable', async (res) => {
+    console.log("Route get /table");
+    try{
+        console.log("Getting secretApiKey");
+        const Authorization = await getSecret(process.env.SECRET_APIKEY); // ApiKey para consumir lambdas
+        
+        const URL = process.env.BACK_BASE + "/taxaprodt";
+        const config = { 
+            headers: { Authorization }
+        }; // Headers a enviar
+        const api = await axios.get(URL, config);
+        const data = await api.data;
+        console.log(data);
+        res.json(data); // Responder los mismos datos que se obtuvieron
+
+        console.log("get success ");
+    } catch (err){
+        if( !err.response ){
+            err.response = {
+                status: 500,
+                data: { error: err.message }
+            }
+        }
+        const { status, data } = err.response;
+        console.log("Error",data);
+        res.status(status).json( data );
+    }
+})
+
+module.exports = router;
