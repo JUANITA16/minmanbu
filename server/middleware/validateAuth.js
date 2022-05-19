d4762121e5808864f635604ad2b189e4ebc9b59f
Colina <Joan.Colina@btgpactual.com>
2022-05-19 18:59:20 -0500

    ADD - automatizacion de rutas

--- server/middleware/validateAuth.js
+++ server/middleware/validateAuth.js
@@ -1,62 +1,23 @@
 const routeGuard = (accessMatrix) => {
     return (req, res, next) => {
-        console.log("Validate auth")
-        if (req.authInfo.roles === undefined) return res.status(403).json({error: 'No user roles found!'});
-        else {
-            const roles = req.authInfo.roles;
-
-            if (req.path.includes(accessMatrix.massive_cc.path)) {
-                if (accessMatrix.massive_cc.methods.includes(req.method)) {
-                    let intersection = accessMatrix.massive_cc.roles.filter(role => roles.includes(role));
-
+        console.log("Inicio middleware de validación")
+        if (req.authInfo.roles === undefined) return res.status(403).json({error: 'User without roles'});
+        const roles = req.authInfo.roles;
+        let path = false;
+        /* Automatico */
+        for(const route in accessMatrix){ // Recorrer cada ruta configurada
+            if (req.path.includes(accessMatrix[route].path)) {
+                path = true;
+                if (accessMatrix[route].methods.includes(req.method)) {
+                    let intersection = accessMatrix[route].roles.filter(role => roles.includes(role));
                     if (intersection.length < 1) return res.status(403).json({error: 'User not allowed'});
+                    break;
                 } 
                 else return res.status(403).json({error: 'Action not allowed'});
             } 
-            else if (req.path.includes(accessMatrix.massive_cdt.path)) {
-                if (accessMatrix.massive_cdt.methods.includes(req.method)) {
-                    let intersection = accessMatrix.massive_cdt.roles.filter(role => roles.includes(role));
-
-                    if (intersection.length < 1) return res.status(403).json({error: 'User not allowed'});
-                } 
-                else return res.status(403).json({error: 'Action not allowed'});
-            }
-            else if (req.path.includes(accessMatrix.table.path)) {
-                if (accessMatrix.table.methods.includes(req.method)) {
-                    let intersection = accessMatrix.table.roles.filter(role => roles.includes(role));
-
-                    if (intersection.length < 1) return res.status(403).json({error: 'User not allowed'});
-                } 
-                else return res.status(403).json({error: 'Action not allowed'});
-            } 
-            else if (req.path.includes(accessMatrix.sap.path)) {
-                if (accessMatrix.sap.methods.includes(req.method)) {
-                    let intersection = accessMatrix.sap.roles.filter(role => roles.includes(role));
-
-                    if (intersection.length < 1) return res.status(403).json({error: 'User not allowed'});
-                } 
-                else return res.status(403).json({error: 'Action not allowed'});
-            }
-            else if (req.path.includes(accessMatrix.tax_a_prodt.path)) {
-                if (accessMatrix.tax_a_prodt.methods.includes(req.method)) {
-                    let intersection = accessMatrix.tax_a_prodt.roles.filter(role => roles.includes(role));
-
-                    if (intersection.length < 1) return res.status(403).json({error: 'User not allowed'});
-                } 
-                else return res.status(403).json({error: 'Action not allowed'});
-            } else if (req.path.includes(accessMatrix.cosif.path)) {
-                if (accessMatrix.cosif.methods.includes(req.method)) {
-                    let intersection = accessMatrix.cosif.roles.filter(role => roles.includes(role));
-
-                    if (intersection.length < 1) return res.status(403).json({error: 'User not allowed'});
-                } 
-                else return res.status(403).json({error: 'Action not allowed'});
-            }
-            else return res.status(403).json({error: 'Unrecognized path'});
         }
-    
+        if(!path) return res.status(403).json({error: 'Unrecognized path'});
         next();
     }
 }
-
 module.exports = routeGuard;
