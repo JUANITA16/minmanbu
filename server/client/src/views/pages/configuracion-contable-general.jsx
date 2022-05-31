3585e56fe6376ee9bd3ac3e30a44cb8e01a2f271
Colina <Joan.Colina@btgpactual.com>
2022-05-31 16:11:02 -0500

    ADD - marcacion de componente error

--- server/client/src/views/pages/configuracion-contable-general.jsx
+++ server/client/src/views/pages/configuracion-contable-general.jsx
@@ -279,7 +279,7 @@ export default function ConfiguracionContableGeneral() {
         top: '50%',
         left: '50%',
         transform: 'translate(-50%, -50%)',
-        width: 1000,
+        width: 1100,
         bgcolor: 'background.paper',
         boxShadow: 24,
         p: 4,
