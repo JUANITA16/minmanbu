6ef2cd7449bf9b975b9b511a45a52d584a309045
Neyra <Brayan.Neyra@btgpactual.com>
2022-07-05 12:47:05 -0500

    Se corrige test de App

--- server/client/src/tests/App.test.js
+++ server/client/src/tests/App.test.js
@@ -1,8 +1,44 @@
 import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom";
 import App from '../App';
+import { BrowserRouter as Router } from "react-router-dom";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+jest.mock("../views/pages/generate-sap", () => () => <mock-widget /> );
+jest.mock("../views/components/navigation-bar", () => () => <mock-widget /> );
+jest.mock("../views/pages/carga-creacion-cuenta-deposito", () => () => <mock-widget /> );
+jest.mock("../views/pages/Logout", () => () => <mock-widget /> );
+jest.mock("../views/pages/configuracion-contable", () => () => <mock-widget /> );
+jest.mock("../views/components/IdleLogout", () => () => <mock-widget /> );
+
+jest.mock("@azure/msal-react", () => ({
+  useMsal: () => ({ 
+      instance:{ 
+          getActiveAccount: jest.fn()
+          .mockReturnValueOnce({ idTokenClaims: { roles: ["MINMAMBU_ROLE_ADMIN"], preferred_username: "test" } })
+          .mockReturnValueOnce({ idTokenClaims: { roles: undefined } })
+      } 
+  }),
+  AuthenticatedTemplate: ({children}) => (<>{children}</>),
+  UnauthenticatedTemplate: ({children}) => (<>{children}</>)
+}));
+
+afterAll(() => {
+  jest.resetAllMocks();
+});
+
+
+beforeAll(() => {
+  process.env.PUBLIC_URL = "";
+});
+
+
+describe('App', () => {
+  test('render home', () => {
+    render( 
+      <Router>
+          <App /> 
+      </Router>
+    );
+    expect(screen.getByText(/Mini mambu página principal./i)).toBeInTheDocument();
+  });  
 });
