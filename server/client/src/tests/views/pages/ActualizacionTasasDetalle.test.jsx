9bd18d7f5dd4c75682d11c2de144103581749c4c
Neyra <Brayan.Neyra@btgpactual.com>
2022-11-28 08:52:58 -0500

    Se agregan pruebas unitarias

--- server/client/src/tests/views/pages/ActualizacionTasasDetalle.test.jsx
+++ server/client/src/tests/views/pages/ActualizacionTasasDetalle.test.jsx
@@ -21,14 +21,14 @@ describe('ActualizacionTasasDetalle', () => {
     test('render ActualizacionTasasDetalle', async () =>  {
         await act( async () => render(<ActualizacionTasasDetalle 
             setIsPantallaPrincipal={setIsPantallaPrincipal}
-            details={details} setdetails={setdetails} />));
+            details={details} setdetails={setdetails} isCuentaCorriente={false}/>));
     })
 
     test('select cant pages', async () =>  {
 
        
         await act( async () => render(<ActualizacionTasasDetalle setIsPantallaPrincipal={setIsPantallaPrincipal}
-            details={details} setdetails={setdetails} />));
+            details={details} setdetails={setdetails}  isCuentaCorriente={false}/>));
 
         await selectEvent.select(screen.getByLabelText('Cantidad de registros'), '20')
         
@@ -37,5 +37,22 @@ describe('ActualizacionTasasDetalle', () => {
         })
     })
 
+    
+    test('Cuenta Corriente', async () =>  {
+        await act( async () => render(<ActualizacionTasasDetalle setIsPantallaPrincipal={setIsPantallaPrincipal}
+            details={details} setdetails={setdetails}  isCuentaCorriente={true}/>));
+
+        expect(screen.getByText(/Nro. Cuenta/i)).toBeInTheDocument();
+    })
+
+    
+    
+    test('Not Cuenta Corriente', async () =>  {
+        await act( async () => render(<ActualizacionTasasDetalle setIsPantallaPrincipal={setIsPantallaPrincipal}
+            details={details} setdetails={setdetails}  isCuentaCorriente={false}/>));
+
+        expect(screen.getAllByText(/Id/i)[0]).toBeInTheDocument();
+        expect(screen.getAllByText(/Estado/i)[0]).toBeInTheDocument();
+    })
 
 })
