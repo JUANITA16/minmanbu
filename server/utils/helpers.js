ebe7ee52e14441425fcd4783d374da47431eb4e5
Neyra <Brayan.Neyra@btgpactual.com>
2023-06-02 15:32:34 -0500

    Se agrega modificacion en middleware

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -3,7 +3,7 @@ const CryptoJs = require("crypto-js");
 
 const decryptText = (mensajeEncriptado) =>{
     var clave = "minmambu-key";
-    const bytes = CryptoJs.AES.decrypt(mensajeEncriptado,clave);
+    const bytes = CryptoJs.AES.decrypt(mensajeEncriptado,clave, { mode: CryptoJS.mode.CTR });
     return bytes.toString(CryptoJs.enc.Utf8);
 }
 
