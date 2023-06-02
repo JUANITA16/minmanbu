346122b50a2cc2fdd035a18a74035e6ba8898735
Neyra <Brayan.Neyra@btgpactual.com>
2023-06-02 15:22:35 -0500

    Se modifica modo de encriptacion a CTR

--- server/client/src/helpers/utils.js
+++ server/client/src/helpers/utils.js
@@ -98,5 +98,5 @@ export function convertMessageError(message){
 export function encryptText(textoOriginal){
     var CryptoJs = require("crypto-js");
     var clave = "minmambu-key";
-    return CryptoJs.AES.encrypt(textoOriginal,clave).toString();
+    return CryptoJs.AES.encrypt(textoOriginal,clave, { mode: CryptoJS.mode.CTR}).toString();
 }
