8661fb25cba969ea7005e47b4e0fecb2983b9b13
Neyra <Brayan.Neyra@btgpactual.com>
2023-06-08 10:54:34 -0500

    Se modifica nombre de variable

--- server/utils/helpers.js
+++ server/utils/helpers.js
@@ -2,7 +2,7 @@ const CryptoJs = require("crypto-js");
 const { getSecret } = require('./secret');
 
 const decryptText = (mensajeEncriptado) =>{
-    var clave = process.env.SECRET_APIKEY; //"minmambu-key";
+    var clave = process.env.PUBLIC_KEY_ENCRYPT; //"minmambu-key";
     const bytes = CryptoJs.AES.decrypt(mensajeEncriptado,clave, { mode: CryptoJs.mode.CTR });
     return bytes.toString(CryptoJs.enc.Utf8);
 }
