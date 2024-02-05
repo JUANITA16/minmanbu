e3a846c6b0ce04cce46d2c96d7bf48e63ae8f318
Santiago Pulgarin <Santiago.Pulgarin@btgpactual.com>
2024-02-05 14:45:32 -0500

    [ADD] added mocks to some tests

---
+++ server/client/src/tests/__mocks__/mocks.js
@@ -0,0 +1,20 @@
+export const emisionTypeMock = [
+    {
+        "updatedate": "2022-10-25T09:48:29-05:00",
+        "producttypemaestrosunicos": "550",
+        "user": "USER_POSTMAN_2",
+        "producttypedescription": "EMISIONES MENOS DE 6 MESES",
+        "producttype": "CDT",
+        "creationdate": "2022-10-25T09:48:29-05:00",
+        "id": "17f423f8-13d6-4441-8241-b9c33e1469b7"
+    },
+    {
+        "updatedate": "2022-10-28T12:56:45-05:00",
+        "producttypemaestrosunicos": "552",
+        "user": "USER_POSTMAN",
+        "producttypedescription": "EMITIDOS IGUAL A 12 MESES Y MENOR DE 18 MESES",
+        "producttype": "CDT",
+        "creationdate": "2022-10-28T12:56:45-05:00",
+        "id": "f524b50f-aa21-420a-8965-756c105a6f91"
+    }
+]
